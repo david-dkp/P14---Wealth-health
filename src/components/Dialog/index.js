3e43b20db4dc7396dbeb7918c2c9c7fe63c60783
David <david.dekeuwer@gmail.com>
2022-10-03 20:33:07 +0200

    feat: create Dialog

---
+++ src/components/Dialog/index.js
@@ -0,0 +1,109 @@
+import PropTypes from "prop-types"
+import { useEffect } from "react"
+import styled from "styled-components"
+import CloseButton from "./CloseButton"
+
+export const Dialog = ({
+    open,
+    onClose,
+    title,
+    children,
+    clickClose,
+    escapeClose,
+    showClose,
+    ...otherProps
+}) => {
+    useEffect(() => {
+        const handleEscape = (event) => {
+            if (event.key === "Escape") {
+                onClose()
+            }
+        }
+
+        if (escapeClose) {
+            document.addEventListener("keydown", handleEscape)
+        }
+
+        return () => {
+            if (escapeClose) {
+                document.removeEventListener("keydown", handleEscape)
+            }
+        }
+    }, [escapeClose, onClose])
+
+    if (!open) return null
+
+    return (
+        <Container
+            onClick={clickClose ? onClose : null}
+            aria-modal="true"
+            aria-hidden={open ? "true" : "false"}
+            role="dialog"
+            {...otherProps}
+        >
+            <Modal
+                onClick={(e) => {
+                    e.stopPropagation()
+                }}
+            >
+                {title && <TitleContainer>{title}</TitleContainer>}
+                <Content>{children}</Content>
+                {showClose && <CloseButton onClick={onClose} />}
+            </Modal>
+        </Container>
+    )
+}
+
+Dialog.propTypes = {
+    open: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
+    title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    children: PropTypes.node.isRequired,
+    clickClose: PropTypes.bool,
+    escapeClose: PropTypes.bool,
+    showClose: PropTypes.bool,
+}
+
+Dialog.defaultProps = {
+    title: null,
+    clickClose: true,
+    escapeClose: true,
+    showClose: true,
+}
+
+const Container = styled.div`
+    z-index: 1000;
+    display: flex;
+    padding: 2rem;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0, 0, 0, 0.5);
+`
+
+const TitleContainer = styled.div`
+    width: 100%;
+    padding: 1em;
+`
+
+const Modal = styled.div`
+    display: flex;
+    position: relative;
+    flex-direction: column;
+    width: 100%;
+    max-width: 600px;
+    max-height: 80vh;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+`
+
+const Content = styled.div`
+    flex: 1;
+    padding: 1em;
+`
--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,19 +1,30 @@
+import { Dialog } from "components/Dialog"
 import useEmployees from "hooks/useEmployees"
-import { useCallback } from "react"
+import { useCallback, useState } from "react"
 import HomePageComponent from "./components/HomePage"
 
 const HomePage = () => {
     const { addEmployee } = useEmployees()
+    const [open, setOpen] = useState(false)
 
     const onSubmit = useCallback(
         (data) => {
-            console.log("submit: " + JSON.stringify(data, null, 3))
-            addEmployee(data).then(() => alert("added employee"))
+            // console.log("submit: " + JSON.stringify(data, null, 3))
+            // addEmployee(data).then(() => alert("added employee"))
+            setOpen(true)
         },
         [addEmployee]
     )
 
-    return <HomePageComponent onSubmit={onSubmit} />
+    return (
+        <>
+            <Dialog open={open} onClose={() => setOpen(false)}>
+                testing here content
+            </Dialog>
+
+            <HomePageComponent onSubmit={onSubmit} />
+        </>
+    )
 }
 
 export default HomePage
